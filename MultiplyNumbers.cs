0c28673c8793aad251851c877c6c2414840c7eb4
AngeleenAbesamis <70412244+AngeleenAbesamis@users.noreply.github.com>
2021-02-18 15:54:08 -0600

    Event Programming Example

---
+++ MultiplyNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    public class MultiplyNumbers
+    {
+        public delegate void d_OddNumber(); //Decalred Delegate
+        public event d_OddNumber e_OddNumber; //Declared Event
+
+        public void Multiply()
+        {
+            Console.WriteLine("Please enter two numbers to multiply: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int answer = n1 * n2;
+            Console.WriteLine(answer.ToString());
+
+            //Check if answer is an odd number, then raise event
+            if((answer % 2 != 0) && (e_OddNumber != null))
+            {
+                e_OddNumber(); //Raised Event
+            }
+        }
+    }
+}
---
+++ SubscriberClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event
+{
+    class SubscriberClass
+    {
+        public static void Main(string[] args)
+        {
+            MultiplyNumbers multiplication = new MultiplyNumbers();
+           
+            //Event gets binded with delegates
+            multiplication.e_OddNumber += new MultiplyNumbers.d_OddNumber(EventMessage);
+            multiplication.Multiply();
+            Console.Read();
+        }
+
+        //Delegates call this method when an event is raised.
+        static void EventMessage()
+        {
+            Console.WriteLine("Event has been executed: This is an Odd Number!");
+        }
+    }
+}
